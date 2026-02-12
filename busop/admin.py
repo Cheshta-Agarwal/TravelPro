4f0faf5ec289d966c5496de22e42dfa492dd39df
Inderjeet Kaur <ikaur1108@gmail.com>
2026-02-12 13:04:28 +0530

    updated model.py and registered model for busop

--- busop/admin.py
+++ busop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Bus, Route, Schedule
+admin.site.register(Bus)
+admin.site.register(Route)  
+admin.site.register(Schedule)
+
