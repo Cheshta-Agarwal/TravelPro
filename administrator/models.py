49c5cd0e559771490a2d526ccb7fec332c3e3b9c
Inderjeet Kaur <ikaur1108@gmail.com>
2026-02-13 10:24:33 +0530

    added admin_user and busop_user model in views.py

--- administrator/models.py
+++ administrator/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class admin_user(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=18)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    OTP = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.username
--- busop/models.py
+++ busop/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+class busop_user(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=18)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    OTP = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.username
+    
+    
 class Bus(models.Model):
     bus_number = models.CharField(max_length=20)
     route = models.CharField(max_length=100)
