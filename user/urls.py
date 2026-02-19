b43e331c67344c12845f01f8ddc53de9e5e5eb8f
Abel <varugheseabel2005@gmail.com>
2026-02-19 10:03:34 +0530

    Added OTP Verification logic and Expiry handling

--- user/urls.py
+++ user/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("logout", views.logout_view, name="logout"),
     path("profile", views.profile, name="profile"),
+    path("otp_login",views.otp_login_view,name="otp_login"),
+    path("otp_verify",views.otp_verify_view,name="otp_verify")
 ]
--- user/views.py
+++ user/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
 from django.conf import settings
+import random
+import time
 
 
 # Create your views here
@@ -65,3 +67,64 @@ def logout_view(request):
 def profile(request):
     return render(request,'profile.html')
 
+def otp_login_view(request):
+    error=None
+
+    if request.method == "POST":
+        email=request.POST.get("email")
+        user=User.objects.filter(email=email).first()
+
+        if not user:
+            error="No user found with this email"
+        else:
+            otp=str(random.randint(100000, 999999))
+            request.session["otp"]=otp
+            request.session["otp_user_id"]=user.id
+            request.session["otp_time"]=time.time()
+
+            send_mail(
+                "Your OTP for TravelPro Login",
+                f"Your OTP for logging into TravelPro is: {otp}. It is valid for 5 minutes.",
+                settings.EMAIL_HOST_USER,
+                [email],
+                fail_silently=False,
+            )
+
+            return redirect("otp_verify")
+    return render(request, "otp_login.html",{"error":error})
+    
+def otp_verify_view(request):
+    error=None
+    if request.method=="POST":
+        otp=request.POST.get("otp")
+        session_otp=request.session.get("otp")
+        user_id=request.session.get("otp_user_id")
+        otp_time=request.session.get("otp_time")
+
+
+        if not session_otp or not user_id or not otp_time:
+            error="OTP session expired. Please try again."
+            return redirect("otp_login")
+        
+        if time.time()-otp_time>300:
+            request.session.pop("otp",None)
+            request.session.pop("otp_user_id",None)
+            request.session.pop("otp_time",None)
+            error="OTP expired. Please request a new one."
+            return redirect("otp_login")
+        
+        if otp!=session_otp:
+            error="Invalid OTP"
+            return render(request,"otp_verify.html",{"error":error})
+        
+        user=User.objects.get(id=user_id)
+        login(request, user)
+
+        request.session.pop("otp",None)
+        request.session.pop("otp_user_id",None)
+        request.session.pop("otp_time",None)
+
+        return redirect("index")
+    return render(request,"otp_verify.html",{"error":error})
+
+    
