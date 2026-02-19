dd53b6556d89b77b3c1e50f7106aaef901d68356
Inderjeet Kaur <ikaur1108@gmail.com>
2026-02-19 12:05:09 +0530

    upadted settings.py

--- TravelPro/settings.py
+++ TravelPro/settings.py
@@ -75,19 +75,17 @@ WSGI_APPLICATION = 'TravelPro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
+
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': 'TravelPro',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': "127.0.0.1",
+        'PORT': '3308',
     }
 }
 
